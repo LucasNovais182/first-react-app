b17874bfc974ea46d0c0306533b19845c9a205cc
Lucas Oliveira Novais de Araújo <42944609+LucasNovais182@users.noreply.github.com>
2019-09-05 01:14:07 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Saudacao from './componentes/Saudacao'
+import Horario from './componentes/Horario'
+
+
+ReactDOM.render(
+    <div>
+    <Saudacao />
+    <Horario />
+    </div> 
+    ,document.getElementById('root'))
